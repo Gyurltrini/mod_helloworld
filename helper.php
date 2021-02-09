ce84938e543a1d5150df4e2d60834d871a47a846
Gyurltrini <kathryn.ma@gmail.com>
2021-02-09 11:13:54 -0400

    Update helper.php

--- helper.php
+++ helper.php
@@ -24,18 +24,41 @@ class ModHelloWorldHelper
     {
         //return 'Hello, World!';
 		// Obtain a database connection
-		$db = JFactory::getDbo();
+		//$db = JFactory::getDbo();
 		// Retrieve the shout. Note that we are now retrieving the id not the lang field.
-		$query = $db->getQuery(true)
-					->select($db->quoteName('hello'))
-					->from($db->quoteName('#__helloworld'))
+		//$query = $db->getQuery(true)
+					//->select($db->quoteName('hello'))
+					//->from($db->quoteName('#__helloworld'))
 					//->where('lang = ' . $db->Quote('en-GB'));
-					->where('id = '. $db->Quote($params));
+					//->where('id = '. $db->Quote($params));
 		// Prepare the query
-		$db->setQuery($query);
+		//$db->setQuery($query);
 		// Load the row.
-		$result = $db->loadResult();
+		//$result = $db->loadResult();
 		// Return the Hello
-		return $result;
+		//return $result;
+	    
+	    // Get a db connection.
+$db = JFactory::getDbo();
+
+// Create a new query object.
+$query = $db->getQuery(true);
+
+// Select all articles for users who have a username which starts with 'a'.
+// Order it by the created date.
+// Note by putting 'a' as a second parameter will generate `#__content` AS `a`
+$query
+    ->select(array('a.*', 'b.username', 'b.name'))
+    ->from($db->quoteName('#__content', 'a'))
+    ->join('INNER', $db->quoteName('#__users', 'b') . ' ON ' . $db->quoteName('a.created_by') . ' = ' . $db->quoteName('b.id'))
+    ->where($db->quoteName('b.username') . ' LIKE ' . $db->quote('a%'))
+    ->order($db->quoteName('a.created') . ' DESC');
+
+// Reset the query using our newly populated query object.
+$db->setQuery($query);
+
+// Load the results as a list of stdClass objects (see later for more options on retrieving data).
+$results = $db->loadObjectList();
+	    return $results;
     }
 }
